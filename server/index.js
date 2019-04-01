ff16af29df9bf2a5281d56e03d7bcea62601fee3
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-04-01 15:25:44 -0500

    Step 7 Done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mc = require("./controllers/messages_controller")
 const app = express();
 app.use(express.json()); 
+app.use(express.static(__dirname + '/../public/build'));
 const port = 3001;
 // const {create, read, update, delete} = require("./controllers/messages_controller")
 
